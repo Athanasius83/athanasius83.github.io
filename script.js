bc01cc5f7256e20c90fd67f822dd51ea2625152a
Athanasius83 <wqrefqw@gmail.com>
2025-03-02 12:32:54 -0600

    grammar + trying to implement highlight on new URL

--- script.js
+++ script.js
@@ -7,22 +7,36 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	var blankDiv = document.getElementsByClassName('filler')[0];
 	blankDiv.style.height = absoluteDivHeight + 5 + "px";
 
-	document.addEventListener("click", function(e){
-		setTimeout(function(e){
-			const url = window.location.href;
-			if (url.lastIndexOf("#")!=-1){
-				const id = url.substring(url.lastIndexOf("#")+1);
-				console.log(id);
-				const footnotes = document.getElementsByClassName("other")[0].children;
-				for (var i = 0; i < footnotes.length; i++){
-					if (footnotes[i].id == id){
-						footnotes[i].style.backgroundColor = "#ADD8E6";
-					} else{
-						footnotes[i].style.backgroundColor = "transparent";
-					}
-				}
-			}
-		}, delay=35);
+	const loadingURL = window.location.href;
 
+	document.addEventListener("click", function(e){
+		setTimeout(highlight, delay=35);
+	});
+	window.addEventListener("keydown",function(e){
+		if (e.key=="Enter"){
+			const newURL = window.location.href;
+			console.log(loadingURL);
+			console.log(newURL);
+			if (!(loadingURL.toString() == newURL.toString())){
+				setTimeout(highlight,delay=35);
+			}
+		}
 	});
 });
+
+function highlight(){
+	const url = window.location.href;
+	if (url.lastIndexOf("#")!=-1){
+		const id = url.substring(url.lastIndexOf("#")+1);
+		console.log(id);
+		const footnotes = document.getElementsByClassName("other")[0].children;
+		for (var i = 0; i < footnotes.length; i++){
+			if (footnotes[i].id == id){
+				footnotes[i].style.backgroundColor = "#ADD8E6";
+			} else{
+				footnotes[i].style.backgroundColor = "transparent";
+			}
+		}
+	}
+
+}
